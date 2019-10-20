5cf76bdc196b5c329c4f640b120a228003ab4448
RickyYWang <ricky.wyp@gmail.com>
2019-10-20 16:50:03 -0700

    distanceranvc

---
+++ RunApp/ViewControllers/DistanceChoose.swift
@@ -0,0 +1,33 @@
+//
+//  DistanceChoose.swift
+//  RunApp
+//
+//  Created by Ricky Wang on 10/20/19.
+//  Copyright © 2019 Michael Peng. All rights reserved.
+//
+
+import UIKit
+
+class DistanceChoose: UIViewController {
+
+    @IBOutlet weak var mile1: UIButton!
+    @IBOutlet weak var mile2: UIButton!
+    @IBOutlet weak var mile3: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
