754b20700766008401d108d32118f1257cd52e18
Michael Peng <mpeng893@student.fuhsd.org>
2019-11-02 16:27:32 -0700

    adsf

---
+++ RunApp/ViewControllers/TimeTrialViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TimeTrialViewController.swift
+//  RunApp
+//
+//  Created by Michael Peng on 11/2/19.
+//  Copyright © 2019 Michael Peng. All rights reserved.
+//
+
+import UIKit
+
+class TimeTrialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        let alert = UIAlertController(title: "Custom Lobby", message: "", preferredStyle: .alert)
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
