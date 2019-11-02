f7fc07a8ccdd98c8a69fa1f7c412786843414497
RickyYWang <ricky.wyp@gmail.com>
2019-11-02 16:45:06 -0700

    fef

--- RunApp/ViewControllers/LoseScreen.swift
+++ RunApp/ViewControllers/LoseScreen.swift
@@ -11,11 +11,15 @@ import Lottie
 import Firebase
 
 class LoseScreen: UIViewController {
-    
+    var ref: DatabaseReference!
+    var phoneNum = ""
     @IBOutlet var runningFlash: AnimationView!
     override func viewDidLoad() {
         super.viewDidLoad()
         startAnimation()
+        retrieveData()
+        
+        ref = Database.database().reference()
         // Do any additional setup after loading the view.
     }
     
@@ -26,6 +30,35 @@ class LoseScreen: UIViewController {
         runningFlash.play()
     }
     @IBAction func goToMessage(_ sender: Any) {
+        let instagramHooks = "sms://1-408-334-5777"
+        let instagramUrl = NSURL(string: instagramHooks)
+        if UIApplication.shared.canOpenURL(instagramUrl! as URL)
+        {
+            UIApplication.shared.openURL(instagramUrl! as URL)
+
+         } else {
+            //redirect to safari because the user doesn't have Instagram
+            UIApplication.shared.openURL(NSURL(string: "http://instagram.com/")! as URL)
+        }
+    }
+    func retrieveData() {
+        ref.child("PlayerStats").child(Auth.auth().currentUser!.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            guard let value = snapshot.value as? NSDictionary else {
+                print("No Data!!!!!!")
+                return
+            }
+            self.phoneNum = value["Phone"] as! String
+        }) { (error) in
+            print("error:\(error.localizedDescription)")
+        }
+        var newString = "1-"
+        for i in 1...10 {
+            if (i == 4 || i == 7) {
+                newString = newString + " "
+            }
+            newString = newString + String(phoneNum[phoneNum.index(phoneNum.startIndex, offsetBy: i-1)])
+        }
     }
     
     
